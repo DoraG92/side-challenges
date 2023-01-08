bfdd277f09847ebf5bdceb8ae29409b5b9991be6
Dora Gores <77057725+DoraG92@users.noreply.github.com>
2023-01-08 15:33:44 +0000

    Add files via upload

---
+++ shoutify.rb
@@ -0,0 +1,21 @@
+# Define a method on string instances, called shoutify. 
+# This method should return the string text in uppercase,
+#  with an extra exclamation mark on the end. 
+# In other words, "hello world".shoutify should return "HELLO WORLD!".
+
+class String 
+  def shoutify 
+    return self.upcase + "!"
+  end
+end
+
+# text = gets.chomp
+text = "hello there cutie"
+puts text.shoutify 
+
+
+class String
+  def bark 
+    return "Woof!"
+  end
+end
